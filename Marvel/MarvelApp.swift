397a6ee0c079764f69f57db2a6c328dd690857ff
Julien Gourdet <julien.gourdet.ext@francetv.fr>
2022-12-06 15:45:17 +0100

    initial commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Julien Gourdet on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
